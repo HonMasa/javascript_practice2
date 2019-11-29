2353085469ef16a0ed6dbbe131a111304e3297db
HonMasa <h.masaya17@gmail.com>
2019-11-30 00:05:25 +0900

    Change toDo.js

--- js/toDo.js
+++ js/toDo.js
@@ -1,91 +1,99 @@
 'use strict';
 
-const allBtn = getElementById("all");
-const workBtn = getElementById("work");
-const doneBtn = getElementById("done");
-const inputText = getElementById("task");
-const addBtn = getElementById("add_button");
-const lists = getElementById("todo_lists");
+const allBtn = document.getElementById("all");
+const workBtn = document.getElementById("work");
+const doneBtn = document.getElementById("done");
+const inputText = document.getElementById("task");
+const addBtn = document.getElementById("add_button");
+const lists = document.getElementById("todo_lists");
 
 const todos = [];
 
-$(function addTask(id, content, status){
-  if (content.length > 0) {
+function addTask(id, content, status){
+  
+  if(content.length > 0){
     const todo = {
       id: id,
       content: content,
       status: status
+      
     };
+    
     todos.push(todo);
   }
-})
+}
 
 
 
-$(function reload(){
+function reload(){
   lists.innerHTML = '';
   makeTask();
-})
+}
 
 
 
-$(function makeTask(){
+function makeTask(){
   const tr = document.createElement('tr');
   const idText = document.createElement('td');
   const contentText =  document.createElement('td');
-  const statusText = document.createElement('td');
   const conditionBtn = document.createElement('button');
   const deleteBtn = document.createElement('button');
-
-  if(todos.length>0){
-    todos.forEach(function(data,index){
+  
+  if(todos.length > 0){
+    
+    todos.forEach(function(todo,index){
       idText.textContent = todo.id;
       contentText.textContent = todo.content;
-      statusText.textContent = todo.status;
       conditionBtn.textContent = todo.status;
       deleteBtn.textContent = "削除";
-
-      conditionBtn.addEventListener('click', () => {
-        if(todo.status === "作業中"){
-          todo.status = "完了";
-        }
-      });
+      console.log(todo);
 
       deleteBtn.addEventListener('click', () => {
-        todos.splice(todo.id+1,1);
+        todos.splice(index,1);
+        reload();
       });
-      
 
-      if(allBtn.checked){
+      conditionBtn.addEventListener('click', () => {
+        todo.status = "完了";
+        reload();
+      });
+
+      if(allBtn.checked === true){
+        
         tr.appendChild(idText);
         tr.appendChild(contentText);
-        tr.appendChild(statusText);
         tr.appendChild(conditionBtn);
-        tr.appendChild(deleteBtn);
+        tr .appendChild(deleteBtn);
+        lists.appendChild(tr);
       }else if(workBtn.checked){
         if(todo.status === "作業中"){
+         
           tr.appendChild(idText);
           tr.appendChild(contentText);
-          tr.appendChild(statusText);
           tr.appendChild(conditionBtn);
-          tr.appendChild(deleteBtn);
+          tr .appendChild(deleteBtn);
+          lists.appendChild(tr);
         }
       }else{
         if(todo.status === "完了"){
+          
           tr.appendChild(idText);
           tr.appendChild(contentText);
-          tr.appendChild(statusText);
           tr.appendChild(conditionBtn);
-          tr.appendChild(deleteBtn);
+          tr .appendChild(deleteBtn);
+          lists.appendChild(tr);
         }
       }
+
+      
     });
   }
 
-})
+}
 
 addBtn.addEventListener('click', () => {
-  addTask(todos.length + 1, inputText, "作業中");
+  
+  addTask(todos.length+1, inputText.value, "作業中");
   reload();
   inputText.value = '';
 });
@@ -104,3 +112,7 @@ workBtn.addEventListener('click', () => {
 doneBtn.addEventListener('click', () => {
   reload();
 });
+
+
+
+
